5925a03f4b85087c17151f6de2cc0158faedb4be
Maariusz Bugajski <mariusz.bugajski@britenet.com.pl>
2023-03-09 14:00:32 +0100

    add dayjs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,18 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
+import dayjs from 'dayjs'
+import 'dayjs/locale/pl'
+import { ref } from 'vue'
 
 const { t } = useI18n()
+
+const dateTime = ref<string>(dayjs().locale('pl').format('DD MMMM YYYY HH:mm'))
 </script>
 
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Teraz mam: {{ dateTime }}</h2>
     <v-alert
       type="success"
       :title="t('hello')"
