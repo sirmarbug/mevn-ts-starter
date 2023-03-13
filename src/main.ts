0fa9dfaab2e6896bb0d3bc0b5b418f1e686214bb
Maariusz Bugajski <mariusz@bugajski.dev>
2023-03-13 09:00:24 +0100

    clear app

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 // Vuetify
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="greetings">
+      <h1 class="text-red">Starter Vue 3 + TS</h1>
+      <h3>
+        You’ve successfully created a project with
+        <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+        <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      </h3>
+    </div>
   </main>
 </template>
