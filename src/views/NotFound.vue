fb1604d56c57a5c07767fbf7f2e062f4d7f73887
Mariusz Bugajski <mariusz@bugajski.dev>
2023-03-14 12:44:25 +0100

    add not found view

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GlobalLayout from '@/layouts/GlobalLayout/GlobalLayout.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const goToApp = () => {
+  router.push({ name: 'StartView' })
+}
+</script>
 
 <template>
-  <h1>Not Found</h1>
+  <GlobalLayout>
+    <v-row no-gutters>
+      <v-col cols="12" sm="4" offset-sm="4" class="d-flex justify-center">
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      </v-col>
+    </v-row>
+    <v-card-title class="d-flex justify-center">Error 404</v-card-title>
+    <v-card-subtitle class="d-flex justify-center">Not Found</v-card-subtitle>
+    <v-card-text class="d-flex justify-center">
+      Nasi programiści już pracują nad naprawieniem awarii. Spróbuj za chwilę ponownie.
+    </v-card-text>
+    <v-card-actions class="d-flex justify-center">
+      <v-btn variant="flat" color="primary" @click="goToApp">Wróc do strony głównej</v-btn>
+    </v-card-actions>
+  </GlobalLayout>
 </template>
-
-<style scoped></style>
