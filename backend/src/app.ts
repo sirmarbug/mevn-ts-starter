11b2f28988e6bebdd3b7ce038d700a10a85c5272
Mariusz Bugajski <mariusz@bugajski.dev>
2023-04-17 13:25:17 +0200

    implement structure project

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import connect from "./configs/db";
+import routes from './routers'
 
 dotenv.config();
 connect();
@@ -11,8 +12,6 @@ const app: Express = express();
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use(routes)
 
 export default app
