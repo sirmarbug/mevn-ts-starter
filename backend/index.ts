4cffcfb97d5a0b0c61990801734903eb84de39c3
Mariusz Bugajski <mariusz@bugajski.dev>
2023-04-17 10:04:48 +0200

    init backend app

---
+++ backend/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || '3000';
+
+app.get('/', (req: Request, res: Response) => {
+  console.log('GET')
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
