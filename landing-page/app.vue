1e33ab9e2a976178c0e16239034c5ad722271e98
Mariusz Bugajski <mariusz@bugajski.dev>
2023-06-12 14:04:04 +0200

    add quasar

--- landing-page/app.vue
+++ landing-page/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <q-btn color="primary" label="Primary" />
+    <QBtn color="secondary" label="Secondary" />
+    <LazyQBtn color="amber" glossy label="Amber" />
   </div>
 </template>
