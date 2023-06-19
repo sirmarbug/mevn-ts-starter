e04d3602ffaeedfc932b7f58a1337fc92416e2fb
Mariusz Bugajski <mariusz@bugajski.dev>
2023-06-19 14:24:03 +0200

    add default layout

---
+++ landing-page/components/DefaultFooter.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <h1>footer</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ landing-page/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {useCounterStore} from "~/composable/useCounter";
+import {storeToRefs} from "pinia";
+
+const store = useCounterStore()
+console.log('store', store)
+const { count, doubleCount } = storeToRefs(store)
+const { increment, decrement, reset } = store
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <h1>Counter: {{ count }}</h1>
+    <h2>Counter double: {{ doubleCount }}</h2>
+    <q-btn color="primary" label="Increment" @click="increment" />
+    <QBtn color="secondary" label="Decrement" @click="decrement" />
+    <LazyQBtn color="amber" glossy label="Reset" @click="reset" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
