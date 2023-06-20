993dfe2ca80d88648d6e5acaeb987bf36ecd452a
Mariusz Bugajski <mariusz@bugajski.dev>
2023-06-20 16:13:17 +0200

    update default layout

--- landing-page/layouts/default.vue
+++ landing-page/layouts/default.vue
@@ -3,9 +3,33 @@
 
 <template>
 <div>
-  <DefaultNavbar/>
-  <slot/>
-  <DefaultFooter/>
+  <q-layout view="lHh lpr lFf" container style="height: 100vh">
+    <q-header>
+      <q-toolbar>
+        <q-avatar class="q-mr-md">
+          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+        </q-avatar>
+        <q-separator dark vertical />
+        <q-toolbar-title>MEVN Starter TS</q-toolbar-title>
+
+        <q-btn stretch flat label="Strona główa" />
+        <q-btn stretch flat label="Opis" />
+        <q-btn stretch flat label="Funkcje" />
+        <q-btn stretch flat label="Kontakt" />
+      </q-toolbar>
+    </q-header>
+    <q-footer>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+    <q-page-container>
+      <q-page class="q-pa-md">
+        <slot/>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </div>
 </template>
 
