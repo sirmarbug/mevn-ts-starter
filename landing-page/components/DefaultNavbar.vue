5b184d3dc199ec283406f8d038f13b9ab26865bc
Mariusz Bugajski <mariusz@bugajski.dev>
2023-06-21 11:04:18 +0200

    implement default navbar

--- landing-page/components/DefaultNavbar.vue
+++ landing-page/components/DefaultNavbar.vue
@@ -2,7 +2,22 @@
 </script>
 
 <template>
-  <h1>NAVBAR</h1>
+  <q-header>
+    <q-toolbar>
+      <q-avatar class="q-mr-md">
+        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+      </q-avatar>
+      <q-separator dark vertical />
+      <q-toolbar-title>MEVN Starter TS</q-toolbar-title>
+
+      <q-btn stretch flat class="lt-md"><IconsMenu/></q-btn>
+      <q-btn stretch flat class="gt-md" label="Strona główa" />
+      <q-btn stretch flat class="gt-md" label="Opis" />
+      <q-btn stretch flat class="gt-md" label="Funkcje" />
+      <q-btn stretch flat class="gt-md" label="Cena" />
+      <q-btn stretch flat class="gt-md" label="Kontakt" />
+    </q-toolbar>
+  </q-header>
 </template>
 
 <style scoped>
--- landing-page/layouts/default.vue
+++ landing-page/layouts/default.vue
@@ -4,20 +4,7 @@
 <template>
 <div>
   <q-layout view="lHh lpr lFf" container style="height: 100vh">
-    <q-header>
-      <q-toolbar>
-        <q-avatar class="q-mr-md">
-          <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-        </q-avatar>
-        <q-separator dark vertical />
-        <q-toolbar-title>MEVN Starter TS</q-toolbar-title>
-
-        <q-btn stretch flat label="Strona główa" />
-        <q-btn stretch flat label="Opis" />
-        <q-btn stretch flat label="Funkcje" />
-        <q-btn stretch flat label="Kontakt" />
-      </q-toolbar>
-    </q-header>
+    <DefaultNavbar/>
     <q-footer>
       <q-toolbar>
         <q-toolbar-title>Footer</q-toolbar-title>
