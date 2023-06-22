4dcd040d95054cfbed7b4f4d40c00d8d62b75e16
Mariusz Bugajski <mariusz@bugajski.dev>
2023-06-22 13:49:50 +0200

    update ui

--- landing-page/components/PricingCard.vue
+++ landing-page/components/PricingCard.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
+interface PricingCardProps {
+  name: string
+  price: number
+  features: string[]
+}
+defineProps<PricingCardProps>()
 </script>
 <template>
-  <q-card flat bordered class="my-card">
+  <q-card flat bordered>
     <q-card-section>
-      <div class="text-h5 text-center">FREE</div>
+      <div class="text-h5 text-center">{{ name }}</div>
     </q-card-section>
 
     <q-separator inset />
 
     <q-card-section class="text-center">
-      <span class="text-h6">$0/month</span>
+      <span class="text-h6">${{ price }}/month</span>
     </q-card-section>
 
     <q-separator inset />
 
     <q-card-section>
-      <p>* 1 active project</p>
-      <p>* 100mb space</p>
-      <p>* ∞ collaborators</p>
+      <p
+          v-for="(feature, index) in features"
+          :key="index"
+      >
+        ✔ {{ feature }}
+      </p>
     </q-card-section>
 
     <q-card-section class="text-center">
--- landing-page/components/Sections/Pricing.vue
+++ landing-page/components/Sections/Pricing.vue
@@ -1,4 +1,33 @@
 <script lang="ts" setup>
+const pricingCards = ref([
+  {
+    name: 'FREE',
+    price: 0,
+    features: [
+        '1 active project',
+        '100mb space',
+        '∞ collaborators'
+    ]
+  },
+  {
+    name: 'PROFESSIONAL',
+    price: 8,
+    features: [
+      '5 active project',
+      '10GB space',
+      '∞ collaborators'
+    ]
+  },
+  {
+    name: 'TEAM',
+    price: 22,
+    features: [
+      '25 active project',
+      '25GB space',
+      '∞ collaborators'
+    ]
+  }
+])
 </script>
 
 <template>
@@ -9,14 +38,16 @@
         <p>Reinforce the reason why people use your product. Lead into the value of each plan before they proceed with their choice.</p>
       </div>
       <div class="row justify-center">
-        <div class="col-sm-12 col-md-2 q-pa-md">
-          <PricingCard/>
-        </div>
-        <div class="col-sm-12 col-md-2 q-pa-md">
-          <PricingCard/>
-        </div>
-        <div class="col-sm-12 col-md-2 q-pa-md">
-          <PricingCard/>
+        <div
+            v-for="(item, index) in pricingCards"
+            :key="index"
+            class="col-sm-12 col-md-2 q-pa-md"
+        >
+          <PricingCard
+            :name="item.name"
+            :price="item.price"
+            :features="item.features"
+          />
         </div>
       </div>
       <div class="col-sm-12 col-md-6 text-center">
